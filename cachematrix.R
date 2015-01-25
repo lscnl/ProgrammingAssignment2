3f5fb46ec79e35f48a9e75ee66df962955ab85b4
lscnl <lsconley@urbanscience.com>
2015-01-25 17:27:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## functions to get and set the inverse (solve) of a matrix 
+## added function to find the determinant
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  chkdet     <- function(det) m <<- det
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       chkdet = chkdet,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
-
+## returns a matrix that is the inverse of x, also checks that the matrix is square and has an inverse
+## use the functions in makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  if(!(nrow(data) == ncol(data))) {
+    message("matrix is not square, unable to solve")
+    return(data)
+  }
+  m <- det(data)
+  if(m == 0) {
+    message("determinant is zero, unable to solve")
+    return(m)
+  }
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
